54a16591a0440b759cb570e5b14cf63b04b996f3
XIONG <xx995b@att.com>
2021-12-23 10:00:21 -0800

    basic game complete, for single iteration

--- src/BlackJackStarter.java
+++ src/BlackJackStarter.java
@@ -23,10 +23,35 @@ public class BlackJackStarter {
         ArrayList<Card> dealerHand = new ArrayList<>();
         Integer playerNum = 0;
         Integer dealerNum = 0;
+        int term = 0;
+        Scanner sc = new Scanner(System.in);
 
         while (true){
-
+            for (Card curCard: allCard){
+                if (term == 0){
+                    playerHand.add(curCard);
+                    term = 1;
+                } else {
+                    dealerHand.add(curCard);
+                    String userResult = "";
+                    if (term == 1){
+                        System.out.println("Your current hand includes: " + playerHand.toString() + ", would you like another card?");
+                        userResult = sc.nextLine();
+                    }
+                    if (userResult.toUpperCase().equals("Y")){
+                        term = 0;
+                    } else {
+                        term = 2;
+                        break;
+                    }
+                }
+            }
         }
+    }
+
+    public static void main(String[] args) {
+        BlackJackStarter newGame = new BlackJackStarter();
+        newGame.startGame();
     }
 
 }
--- src/Card.java
+++ src/Card.java
@@ -6,4 +6,27 @@ public class Card {
         this.curSuit = curSuit;
         this.curRank = curRank;
     }
+
+    @Override
+    public String toString() {
+        String curRankDispay = "";
+        switch (curRank){
+            case 1:
+                curRankDispay += "Ace";
+                break;
+            case 11:
+                curRankDispay += "J";
+                break;
+            case 12:
+                curRankDispay += "Q";
+                break;
+            case 13:
+                curRankDispay += "K";
+                break;
+            default:
+                curRankDispay += curRank;
+                break;
+        }
+        return curSuit + " " + curRankDispay;
+    }
 }
